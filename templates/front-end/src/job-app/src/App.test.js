427459494c0161590185069531991b1136b98952
Filippo <guerrierofilippo3 @mail.com>
2025-11-20 17:41:05 +0100

    Modifiche per risolvere issue di sonarqube

--- templates/front-end/src/job-app/src/App.test.js
+++ templates/front-end/src/job-app/src/App.test.js
@@ -9,15 +9,21 @@ describe('App Component', () => {
 
   it('renders data after fetch', async () => {
     render(<App />);
-    await waitFor(() => expect(screen.getByText("Sviluppatore")).toBeInTheDocument());
-    expect(screen.getByText("Designer")).toBeInTheDocument();
+    // Aspetta che venga mostrato "Project-Test", che è nel mock
+    await waitFor(() => expect(screen.getByText("Project-Test")).toBeInTheDocument());
+    expect(screen.getByText("DONE")).toBeInTheDocument();
   });
 
   it('renders the table headers', async () => {
     render(<App />);
     await waitFor(() => {
-      expect(screen.getByText("job")).toBeInTheDocument();
-      expect(screen.getByText("company")).toBeInTheDocument();
+      // Usa le chiavi dell'oggetto mock come intestazioni
+      expect(screen.getByText("id")).toBeInTheDocument();
+      expect(screen.getByText("project")).toBeInTheDocument();
+      expect(screen.getByText("startdate")).toBeInTheDocument();
+      expect(screen.getByText("enddate")).toBeInTheDocument();
+      expect(screen.getByText("status")).toBeInTheDocument();
+      expect(screen.getByText("data")).toBeInTheDocument();
     });
   });
 });

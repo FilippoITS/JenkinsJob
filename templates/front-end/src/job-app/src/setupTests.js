34926101742d7097045e3582a13ffbac6e4aad1f
Filippo <guerrierofilippo3 @mail.com>
2025-11-20 17:49:28 +0100

    Modifiche per risolvere issue di sonarqube

--- templates/front-end/src/job-app/src/setupTests.js
+++ templates/front-end/src/job-app/src/setupTests.js
@@ -1,17 +1,19 @@
 // src/setupTests.js
 console.log("setupTests.js LOADED");
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve([
-      {
-        id: 1,
-        project: "Project-Test",
-        startdate: "2024-01-01T10:00:00",
-        enddate: "2024-01-01T12:00:00",
-        status: "DONE",
-        data: "Sample data"
-      }
-    ])
-  })
-);
+if (!globalThis.fetch) {
+  globalThis.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve([
+
+  {
+          id: 1,
+          project: "Project-Test",
+          startdate: "2024-01-01T10:00:00",
+          enddate: "2024-01-01T12:00:00",
+          status: "DONE",
+          data: "Sample data"
+            }
+
+        ])
+    })
+  );
+}

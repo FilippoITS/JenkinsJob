93b785c3baf13aab491e14f33e77ebc45fc09aba
Filippo <guerrierofilippo3 @mail.com>
2025-11-20 15:34:03 +0100

    Modifiche per risolvere issue di sonarqube

--- templates/back-end/src/job/src/test/java/com/job/utilities/ConversionTest.java
+++ templates/back-end/src/job/src/test/java/com/job/utilities/ConversionTest.java
@@ -9,14 +9,10 @@ import java.time.LocalDateTime;
 
 public class ConversionTest {
 
-    LocalDateTime startDate = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
-    LocalDateTime endDate = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
-
     @Test
     public void testConvertToDTO() {
-        // Crea un oggetto Job con dati di esempio
-        Job job = new Job(1, "Project A", startDate, endDate, "In Progress", "Some job data");
-
+        // Crea un oggetto Job con i parametri giusti
+        Job job = new Job(1, "Project A", LocalDateTime.now(), LocalDateTime.now(), "In Progress", "Some job data");
 
         // Esegui la conversione
         JobDTO jobDTO = Conversion.convertToDTO(job);
@@ -29,5 +25,4 @@ public class ConversionTest {
         assertEquals(job.getStatus(), jobDTO.getStatus());
         assertEquals(job.getData(), jobDTO.getData());
     }
-
 }
